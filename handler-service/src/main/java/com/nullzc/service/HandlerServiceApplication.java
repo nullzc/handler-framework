2dede8daa524c4c100bb6b3aa9e0b0580015fd49
nullzc <nullzc@163.com>
2019-05-30 21:19:52 +0800

    骨架

---
+++ handler-service/src/main/java/com/nullzc/service/HandlerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nullzc.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlerServiceApplication.class, args);
+	}
+
+}
