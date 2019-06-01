a72e6f2676e59b32b374c4fcfbdf09f2eb1a71ab
nullzc <nullzc@163.com>
2019-06-01 17:03:40 +0800

    骨架

---
+++ handler-common/src/main/java/com/nullzc/common/Test.java
@@ -0,0 +1,14 @@
+package com.nullzc.common;
+
+/**
+ * @Description TODO
+ * @Author nullzc
+ * @Date 2019/6/1
+ */
+public class Test {
+
+    public String getName(String name) {
+        System.err.println(String.format("==========%s========", name));
+        return name;
+    }
+}
---
+++ handler-service/src/main/java/com/nullzc/service/HandlerServiceApplication.java
@@ -0,0 +1,14 @@
+package com.nullzc.service;
+
+/**
+ * @Description main类
+ * @Author nullzc
+ * @Date 2019/6/1
+ */
+
+public class HandlerServiceApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
